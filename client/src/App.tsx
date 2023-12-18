0fb3833f5295c57a69982bfb34aaf95f3b36b9db
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2023-12-18 20:53:14 +0100

    Add search and basket to the header. Added footer compoent

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
+import Footer from './Components/Partial/Footer'
 import Header from './Components/Partial/Header'
 import './index.css'
 
 const App = () => {
   return (
     <>
-    <Header />
+      <Header />
       <h1 className='text-desktop-title font-heading text-deep-red font-extrabold underline'>
         HELLO WORLD!
       </h1>
       <p className='text-sub-title font-text'>OUR PRODUCTS</p>
+      <Footer />
     </>
   )
 }
--- client/src/Components/Partial/Header.tsx
+++ client/src/Components/Partial/Header.tsx
@@ -1,17 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBars,
+  faMagnifyingGlass,
+  faBasketShopping,
+} from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import logo from '../../assets/images/coffeebean_logo.png'
 
 const Header = () => {
   return (
     <div className='container fixed bg-deep-brown w-screen'>
-      <p className='text-white font-features text-center'>
+      <p className='text-white font-features text-center p-1'>
         We're giving 5% of your order to the planet
       </p>
-      <div className='bg-deep-red flex align-middle'>
+      <div className='bg-deep-red flex align-middle items-center justify-between px-2 py-1'>
         <FontAwesomeIcon icon={faBars} className='text-white text-4xl' />
-        <img src={logo} alt='coffebean logo' className='w-40 p-3' />
+        <img src={logo} alt='coffebean logo' className='w-40' />
+        <div className='flex gap-4'>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className='text-white text-4xl'
+          />
+          <FontAwesomeIcon
+            icon={faBasketShopping}
+            className='text-white text-4xl'
+          />
+        </div>
       </div>
     </div>
   )
