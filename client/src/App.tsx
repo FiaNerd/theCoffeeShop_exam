78a7b75cf31718bf5cbdbba068bf9305c01b0472
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2023-12-18 17:39:20 +0100

    Add config tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,12 @@ import './index.css'
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-amber-700 font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className='text-size-desktop-title font-heading text-dark-red font-extrabold underline'>
+        HELLO WORLD!
+      </h1>
+      <p className='text-size-sub-title font-text'>Our products</p>
+    </>
   )
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-red': '#42201A',
+        dark: '#221E1F',
+        brown: '#332620',
+        'dark-brown': '#2A221D',
+        'medium-brown': '#675C60',
+        'light-brown': '#8D7869',
+        purple: '#3F333A',
+        'light-purple': '#675C60',
+      },
+      fontFamily: {
+        heading: ['Bebas Neue Pro',  'Helvetica'],
+        text: ['Popins', 'Arial', 'sans-serif'],
+
+      },
+      fontSize: {
+        'size-title': '1.875rem',
+        'size-sub-title': '1.25rem',
+        'size-paragraph': '1rem',
+        'size-feautures': '0.875rem',
+        'size-desktop-title': '2.25rem',
+        'size-desktop-sub-title': '1.5rem',
+        'size-desktop-paragraph': '1.125rem',
+        'size-desktop-feautures': '1rem',
+      },
+    },
   },
   plugins: [],
 }
-
