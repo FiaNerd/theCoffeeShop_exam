1df8e83d00b83d986433dbe0ebe450b5e15cd4a4
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2023-12-19 23:58:46 +0100

    Add here section image under header

---
+++ client/src/Components/HeroSection.tsx
@@ -0,0 +1,7 @@
+import heroImg from '../assets/images/hero_section.jpg'
+
+const HeroSection = () => {
+  return <img src={heroImg} alt='hero image' className='hero-section' />
+}
+
+export default HeroSection
--- client/src/Components/Partial/Header.tsx
+++ client/src/Components/Partial/Header.tsx
@@ -1,18 +1,27 @@
-import Navbar from './Navbar'
+import { useLocation } from 'react-router-dom';
+import HeroSection from '../HeroSection';
+import Navbar from './Navbar';
 
 const Header = () => {
-  
-  return (
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+return (
     <>
-      <div className='bg-deep-brown sticky'>
+      <div className='fixed top-0 left-0 w-full bg-deep-brown'>
         <p className='text-white font-features text-center p-1'>
           We're giving 5% of your order to the planet
         </p>
-
-        <Navbar />
+            <Navbar />
       </div>
+        <div style={{
+                marginTop: '128.641px',
+              }}>
+        {isHomePage && <HeroSection />}
+        
+        </div>
     </>
-  )
-}
+  );
+};
 
 export default Header
