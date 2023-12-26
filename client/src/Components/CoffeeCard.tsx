0468aa6d5e82504a8f0d058ac9c79899432a912c
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2023-12-26 11:55:55 +0100

    Add reusable button and added it to the Product Card

--- client/src/Components/CoffeeCard.tsx
+++ client/src/Components/CoffeeCard.tsx
@@ -1,5 +1,6 @@
 import { Link, useParams } from 'react-router-dom'
 import { Product } from '../types/ProductsAPI.types'
+import Button from './Partial/Button'
 
 interface IProps {
   product: Product
@@ -37,21 +38,15 @@ const CoffeeCard = ({ product }: IProps) => {
           </p>
 
           <div className='flex flex-col w-full justify-between items-center mt-4 sm:mt-0'>
-            <button className='flex items-center justify-center gap-4 bg-orange mb-4 w-full py-3 rounded  hover:opacity-80'>
-              <svg width='22' height='20' xmlns='http://www.w3.org/2000/svg'>
-                <path
-                  d='M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z'
-                  fill='white'
-                />
-              </svg>
-              <span className='text-white font-bold uppercase'>Lägg till</span>
-            </button>
+            <Button buttonType='create' typeAction='submit' iconType='cart'>
+              Lägg till
+            </Button>
 
             <div className='w-full'>
               <Link to={`/product/${type}/${product.productId}`}>
-                <button className='text-center border-2 w-full text-dark-deep-brown font-bold uppercase border-orange py-3 rounded hover:opacity-80'>
+                <Button buttonType='read-more' typeAction='button'>
                   Läs mer
-                </button>
+                </Button>
               </Link>
             </div>
           </div>
