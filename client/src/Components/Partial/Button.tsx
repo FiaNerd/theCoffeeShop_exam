d89406c9a37d8d35e921aea74feeafa3521d268b
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2023-12-26 19:14:39 +0100

    Add basket slide for see basket details and checking out proucts, fixed a basket counter

--- client/src/Components/Partial/Button.tsx
+++ client/src/Components/Partial/Button.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import {
+  faPlus,
+  faMinus
+} from '@fortawesome/free-solid-svg-icons'
 import clsx from 'clsx'
 
 interface IProps {
