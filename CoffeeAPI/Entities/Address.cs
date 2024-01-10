8febb6d410b92da2e9f3a8a7e7dff57dc619a36f
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2024-01-10 17:58:51 +0100

    Add Address class for order

---
+++ CoffeeAPI/Entities/Address.cs
@@ -0,0 +1,11 @@
+namespace CoffeeAPI.Entities
+{
+    public class Address
+    {
+        public string FullName { get; set;}
+        public string Adress1 { get; set; }
+        public string Adress2 { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+    }
+}
