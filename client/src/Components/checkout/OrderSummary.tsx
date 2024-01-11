1dbcd6ee31bb152492d7e5f55a5217be94388bbf
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2024-01-11 10:14:06 +0100

    Add order summery

---
+++ client/src/Components/checkout/OrderSummary.tsx
@@ -0,0 +1,60 @@
+import StepCounter from "../partial/StepCounter";
+
+const OrderSummary = () => {
+    return (           
+    <div className="grid mx-auto max-w-[40em]">
+        <StepCounter />
+        <div className="col-span-1 ">
+            <h1 className="py-6 border-b-2 text-deep-brown px-8">Order Summary</h1>
+            <ul className="py-6 border-b space-y-6 px-8">
+                <li className="grid grid-cols-6 gap-2 border-b-1">
+                    <div className="col-span-1 self-center">
+                        <img src="/images/products/bryggkaffe/mork_rost/froza-1200x1200.jpg" alt="Product" className="rounded w-full" />
+                    </div>
+                    <div className="flex flex-col col-span-3 pt-2">
+                        <span className="text-gray-600 text-md font-semi-bold">Studio 2 Headphone</span>
+                        <span className="text-gray-400 text-sm inline-block pt-2">Red Headphone</span>
+                    </div>
+                    <div className="col-span-2 pt-3">
+                        <div className="flex items-center space-x-2 text-sm justify-between">
+                            <span className="text-gray-400">2 </span>
+                            <span className="text-deep-brownfont-semibold inline-block">61.98 kr</span>
+                        </div>
+                    </div>
+                </li>
+                <li className="grid grid-cols-6 gap-2 border-b-1">
+                    <div className="col-span-1 self-center">
+                        <img src="/images/products/bryggkaffe/mork_rost/estanzia_organic-1200x1200.jpg" alt="Product" className="rounded w-full" />
+                    </div>
+                    <div className="flex flex-col col-span-3 pt-2">
+                        <span className="text-gray-600 text-md font-semi-bold">Apple iPhone 13</span>
+                        <span className="text-gray-400 text-sm inline-block pt-2">Phone</span>
+                    </div>
+                    <div className="col-span-2 pt-3">
+                        <div className="flex items-center space-x-2 text-sm justify-between">
+                            <span className="text-gray-400">1</span>
+                            <span className="text-deep-brown font-semibold inline-block">785 kr</span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div className="px-8 border-b">
+                <div className="flex justify-between py-4 text-gray-600">
+                    <span>Subtotal</span>
+                    <span className="font-semibold text-deep-brown">846.98 kr</span>
+                </div>
+                <div className="flex justify-between py-4 text-gray-600">
+                    <span>Shipping</span>
+                    <span className="font-semibold text-orange">Gratis frakt</span>
+                </div>
+            </div>
+            <div className="font-semibold text-xl px-8 flex justify-between py-8 text-deep-brown">
+                <span>Total</span>
+                <span>€846.98</span>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default OrderSummary
--- client/src/Pages/CheckoutPage.tsx
+++ client/src/Pages/CheckoutPage.tsx
@@ -1,8 +1,45 @@
-import AddressForm from "../components/checkout/AddressForm";
+
+import { useState } from "react"
+import { FormProvider, useForm } from "react-hook-form"
+import AddressForm from "../components/checkout/AddressForm"
+import OrderSummary from "../components/checkout/OrderSummary"
+import PaymentForm from "../components/checkout/PaymentForm"
+
+const step =['Leverans adress', 'Se din order', 'Betailnings detaljer']
+
+const getStepContent = (step: number) => {
+  switch (step) {
+    case 0:
+      return <AddressForm />
+    case 1:
+      return <ReviewOrder />
+    case 2:
+      return <PaymentForm />
+    default:
+      throw new Error('Unknown step')
+  }
+}
+
 
 const CheckoutPage = () => {
+  const methods = useForm() 
+  const [ activeStep, setActiveStep ] = useState(0)
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1)
+  }
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1)
+  } 
+
   return (
-    <AddressForm />
+    <>
+    <FormProvider {...methods}>
+        {/* <AddressForm /> */}
+        <OrderSummary />
+      </FormProvider>
+    </>
   )
 }
 
