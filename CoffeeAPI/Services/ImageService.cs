cc54f231697aae1995a1af2cdbda62396d428892
mattiasson.sofia@icloud.com <mattiasson.sofia@icloud.com>
2024-01-13 14:48:20 +0100

    Add a configuration for my account to cloudflare and added logic for upload images

--- CoffeeAPI/Services/ImageService.cs
+++ CoffeeAPI/Services/ImageService.cs
@@ -1,10 +1,44 @@
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
 namespace CoffeeAPI.Services
 {
     public class ImageService
     {
+        private readonly Cloudinary _cloudinary;
+
+        // Config to cloudinary
         public ImageService(IConfiguration config)
         {
-            
+            // Comes from cloudinary, my specifik account
+            var acc = new Account
+            (
+                config["Cloudinary:CloudName"],
+                config["Cloudinary:ApiKey"],
+                config["Cloudinary:ApiSecret"]
+            );
+
+            // Giving acces to cloudinary service
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> AddImageAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+            using var stream = file.OpenReadStream();
+
+            var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream)
+                };
+                
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+
+            return uploadResult;
         }
     }
 }
